78355d36d64daaa9de2897d7966ef5ef812168f6
Ahmed Bodiwala <ahmed_bodi@msn.com>
2018-07-11 01:49:46 +0100

    Bump version: 0.2.0 → 0.2.1

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def read(*names, **kwargs):
 
 setup(
     name='genesiscreator',
-    version='version='0.2.0'',
+    version='version='0.2.1'',
     license='BSD 3-Clause License',
     description='A Crypto-Currency Genesis Block Creator',
     long_description='%s\n%s' % (
--- src/genesiscreator/__init__.py
+++ src/genesiscreator/__init__.py
@@ -1,2 +1,2 @@
 """Package for the GenesisCreator Library."""
-__version__ = '__version__ = '0.2.0''
+__version__ = '__version__ = '0.2.1''
